9c3b9bb83c14b894bebb3a30431db3010e775cff
Diego Rios-Rojas <riosdiego1999@gmail.com>
2025-06-04 14:06:41 -0400

    updated replit config to properly load playwright

--- replit.nix
+++ replit.nix
@@ -15,25 +15,25 @@
         pkgs.gcc
         pkgs.nodejs
 
-        # Missing libraries for Playwright - mapped to Nix packages
-        pkgs.nss              # provides libnss3.so, libnssutil3.so, libsmime3.so
-        pkgs.nspr             # provides libnspr4.so
-        pkgs.dbus             # provides libdbus-1.so.3
-        pkgs.atk              # provides libatk-1.0.so.0
-        pkgs.at-spi2-atk      # provides libatk-bridge-2.0.so.0
-        pkgs.cups             # provides libcups.so.2
-        pkgs.expat            # provides libexpat.so.1
-        pkgs.libxcb           # provides libxcb.so.1
-        pkgs.libxkbcommon     # provides libxkbcommon.so.0
-        pkgs.at-spi2-core     # provides libatspi.so.0
-        pkgs.xorg.libXcomposite  # provides libXcomposite.so.1
-        pkgs.xorg.libXdamage     # provides libXdamage.so.1
-        pkgs.xorg.libXfixes      # provides libXfixes.so.3
-        pkgs.mesa             # provides libgbm.so.1
-        pkgs.pango            # provides libpango-1.0.so.0
-        pkgs.cairo            # provides libcairo.so.2
-        pkgs.systemd          # provides libudev.so.1
-        pkgs.alsa-lib         # provides libasound.so.2
+        # Playwright dependencies
+        pkgs.nss
+        pkgs.nspr
+        pkgs.dbus
+        pkgs.atk
+        pkgs.at-spi2-atk
+        pkgs.cups
+        pkgs.expat
+        pkgs.libxcb
+        pkgs.libxkbcommon
+        pkgs.at-spi2-core
+        pkgs.xorg.libXcomposite
+        pkgs.xorg.libXdamage
+        pkgs.xorg.libXfixes
+        pkgs.mesa
+        pkgs.pango
+        pkgs.cairo
+        pkgs.systemd
+        pkgs.alsa-lib
     ];
     env = {
         PYTHONBIN = "${pkgs.python311Full}/bin/python3.11";
